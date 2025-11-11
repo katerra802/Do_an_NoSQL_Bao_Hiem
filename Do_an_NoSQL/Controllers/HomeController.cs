2b22d0fff4081d6a7547289bef86e58a5ad55cc2
katerra802 <93852944+katerra802@users.noreply.github.com>
2025-11-11 09:02:57 +0700

    f

---
+++ Do_an_NoSQL/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Do_an_NoSQL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_an_NoSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Do_an_NoSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Do_an_NoSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
