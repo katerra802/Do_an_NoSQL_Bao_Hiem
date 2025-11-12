5f0cfb0a7252187aa7954dab52cd85ee7982acf0
katerra802 <93852944+katerra802@users.noreply.github.com>
2025-11-12 13:13:04 +0700

    x

--- Do_an_NoSQL/Database/MongoDbContext.cs
+++ Do_an_NoSQL/Database/MongoDbContext.cs
@@ -13,12 +13,42 @@ namespace Do_an_NoSQL.Database
             _database = client.GetDatabase(settings.Value.DatabaseName);
         }
         public IMongoCollection<Customer> Customers
-            => _database.GetCollection<Customer>("Customers");
+            => _database.GetCollection<Customer>("customers");
 
         public IMongoCollection<Advisor> Advisors
-            => _database.GetCollection<Advisor>("Advisors");
+            => _database.GetCollection<Advisor>("advisors");
 
         public IMongoCollection<Product> Products
-            => _database.GetCollection<Product>("Products");
+            => _database.GetCollection<Product>("products");
+
+        public IMongoCollection<PolicyApplication> PolicyApplications
+            => _database.GetCollection<PolicyApplication>("policy_applications");
+
+        public IMongoCollection<Policy> Policies
+            => _database.GetCollection<Policy>("policies");
+
+        public IMongoCollection<UnderwritingDecision> UnderwritingDecisions
+            => _database.GetCollection<UnderwritingDecision>("underwriting_decisions");
+
+        public IMongoCollection<Beneficiary> Beneficiaries
+            => _database.GetCollection<Beneficiary>("beneficiaries");
+
+        public IMongoCollection<PaymentSchedule> PaymentSchedules
+            => _database.GetCollection<PaymentSchedule>("payment_schedules");
+
+        public IMongoCollection<PremiumPayment> PremiumPayments
+            => _database.GetCollection<PremiumPayment>("premium_payments");
+
+        public IMongoCollection<Claim> Claims
+            => _database.GetCollection<Claim>("claims");
+
+        public IMongoCollection<ClaimPayout> ClaimPayouts
+            => _database.GetCollection<ClaimPayout>("claim_payouts");
+
+        public IMongoCollection<Role> Roles
+            => _database.GetCollection<Role>("roles");
+
+        public IMongoCollection<User> Users
+            => _database.GetCollection<User>("users");
     }
 }
